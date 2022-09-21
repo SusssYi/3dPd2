41c6ff33618467f6c0889e7a36a02e1a5642ab22
Sus <qq945792171@gmail.com>
2022-09-21 12:27:53 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,28 +5,30 @@ import { useTheme } from "next-themes";
 import { Suspense } from "react";
 
 const Home: NextPage = () => {
-  const { theme, setTheme } = useTheme();
+    const { theme, setTheme } = useTheme();
 
-  return (
-    <Suspense fallback="loading...">
-      <NextSeo title="first" description=" some desc" />
+    return (
+        <Suspense fallback="loading...">
+            <NextSeo title="first" description=" some desc" />
 
-      <div className="container-col space-y-8 ">
-        <div className="prose dark:prose-dark">
-          <h1 className="text-3xl px-8 py-2 rounded-2xl transition-all duration-100  bg-teal-300 dark:bg-red-400  ">
-            Welcome to Your App
-          </h1>
-        </div>
+            <div className="container-col space-y-8 ">
+                <div className="prose dark:prose-dark">
+                    <h1 className="text-3xl px-8 py-2 rounded-2xl transition-all duration-100  bg-teal-300 dark:bg-red-400  ">
+                        Welcome to Your App
+                    </h1>
+                </div>
 
-        <button
-          className="btn-base "
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          toggle
-        </button>
-      </div>
-    </Suspense>
-  );
+                <button
+                    className="btn-base "
+                    onClick={() =>
+                        setTheme(theme === "dark" ? "light" : "dark")
+                    }
+                >
+                    toggle
+                </button>
+            </div>
+        </Suspense>
+    );
 };
 
 export default Home;
