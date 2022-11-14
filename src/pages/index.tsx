7e7759b76f57ee90df59209d8307e99547644d51
Sus <qq945792171@gmail.com>
2022-11-14 18:50:00 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,16 @@ import TopSection from "../components/TopSection";
 const Home: NextPage = () => {
     return (
         <div className=" flex w-full min-h-screen h-auto flex-col py-12 md:py-0">
+            <head>
+                <title>Unfinished</title>
+            </head>
+
+            {/* TODO: add animation svg or 3d scene */}
             <Header />
             <TopSection />
             <Technologies />
+            {/* TODO:work section */}
+            {/* TODO:email section */}
         </div>
     );
 };
